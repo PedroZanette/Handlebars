7aa09c49ca8b2b29261e87407cab910a17b55e7b
PEDRO HENRIQUE NUNES ZANETTE <pedro_zanette@estudante.sesisenai.org.br>
2024-10-08 09:28:51 -0300

    initial commit

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ const app = express()
 app.engine("handlebars", exphbs.engine())
 app.set('view engine', 'handlebars') //colocar uma variavel que armazena temporiariamente
 
+app.get('/peixe', (req, res) => {
+    res.render('peixe')
+})
+
 app.get('/', (req, res) => { //associa uma poprieadade a uma função
     const user = {
         name: 'Karien',
         age: 34,
         email: 'karien.barbosa@edu.sc.senai.br'
     }
-    res.render('acai', {user: user}) //envia dados do acai para armazenamento em nuvem
+    const auth = true
+
+    res.render('acai', {user: user, auth}) //envia dados do acai para armazenamento em nuvem
 
 })
 
